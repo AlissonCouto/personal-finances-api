22702bcf87876193808349b2b1c685a95cc4bf8e
Alisson Couto <allison.couto@phooto.com.br>
2025-07-17 20:00:32 -0400

    chore: Adicionando docker-compose com Postgres e configurando Prisma ORM

---
+++ prisma/migrations/20250717235727_create_all_tables/migration.sql
@@ -0,0 +1,98 @@
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('BankSlip', 'CreditCard', 'DebitCard', 'Check', 'Cash', 'BankPayment', 'Courtesy', 'Voucher', 'Agreement');
+
+-- CreateTable
+CREATE TABLE "House" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "House_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "house_id" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "percentage" DECIMAL(9,2) NOT NULL DEFAULT 0,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Income" (
+    "id" SERIAL NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "due_date" TIMESTAMP(6) NOT NULL,
+    "amount" DECIMAL(9,2) NOT NULL,
+    "payment_method" "PaymentMethod" NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "house_id" INTEGER NOT NULL,
+    "category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Income_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" SERIAL NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "due_date" TIMESTAMP(6) NOT NULL,
+    "amount" DECIMAL(9,2) NOT NULL,
+    "payment_method" "PaymentMethod" NOT NULL,
+    "is_paid" BOOLEAN NOT NULL,
+    "paid_amount" DECIMAL(9,2),
+    "payment_date" TIMESTAMP(6),
+    "discount" DECIMAL(9,2),
+    "fees" DECIMAL(9,2),
+    "createdAt" TIMESTAMP(6) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "house_id" INTEGER NOT NULL,
+    "category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_house_id_fkey" FOREIGN KEY ("house_id") REFERENCES "House"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Income" ADD CONSTRAINT "Income_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Income" ADD CONSTRAINT "Income_house_id_fkey" FOREIGN KEY ("house_id") REFERENCES "House"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Income" ADD CONSTRAINT "Income_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_house_id_fkey" FOREIGN KEY ("house_id") REFERENCES "House"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
