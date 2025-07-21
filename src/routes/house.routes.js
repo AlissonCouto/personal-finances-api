d76f7418f933c1aec2b23f09b31c575ba572d5e8
AlissonCouto <alissonvieiracaires@gmail.com>
2025-07-20 21:37:18 -0400

    feat: Renomeando as rotas do CRUD de casas para o plural

--- src/routes/house.routes.js
+++ src/routes/house.routes.js
@@ -1,11 +1,11 @@
 import { create, get, getId, update, remove } from '../controllers/house.controller.js';
 
 const houseRoutes = app => {
-    app.post('/house', create);
-    app.get('/house', get);
-    app.get('/house/:id', getId);
-    app.put('/house/:id', update);
-    app.delete('/house/:id', remove);
+    app.post('/houses', create);
+    app.get('/houses', get);
+    app.get('/houses/:id', getId);
+    app.put('/houses/:id', update);
+    app.delete('/houses/:id', remove);
 }
 
 export default houseRoutes;
